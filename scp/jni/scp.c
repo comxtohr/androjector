cff0709a295a0d233f746791ba4ae012f8b9cab8
comxtohr <somhtong@gmail.com>
2013-08-13 17:57:45 +0800

    ver 1.6

--- scp/jni/scp.c
+++ scp/jni/scp.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void process(int,int,int);
+void process(int,int,int,int);
 int main()
 {
 	int buf[3];		//buf[0] : width
@@ -9,23 +9,22 @@ int main()
 	fread(buf,4,3,stdin);
 	buf[0] /= 2;
 	buf[1] /= 2;
-	fwrite(buf,4,3,stdout);
+	fwrite(buf,4,2,stdout);
 	if (buf[2] == 1 || buf[2] == 2 || buf[2] == 5)
-		process(buf[0] * 2,buf[1] * 2,4);	//32-bit
+		process(buf[0] * 2,buf[1] * 2,4,buf[2]);	//32-bit
 	else if (buf[2] == 3)
-		process(buf[0] * 2,buf[1] * 2,3);	//24-bit
-	else if (buf[2] == 4 || buf[2] == 6 || buf[2] == 7 || buf[2] == 10)
-		process(buf[0] * 2,buf[1] * 2,2);	//16-bit
-	else if (buf[2] == 8 || buf[2] == 9 || buf[2] == 11)
-		process(buf[0] * 2,buf[1] * 2,1);	//8-bit
+		process(buf[0] * 2,buf[1] * 2,3,buf[2]);	//24-bit
+	else if (buf[2] == 4 || buf[2] == 6 || buf[2] == 7)
+		process(buf[0] * 2,buf[1] * 2,2,buf[2]);	//16-bit
 	else
 		fprintf(stderr,"can't process this pixel format #%d\n",buf[2]);
 	return 0;
 }
 
-void process(int width,int height,int bits)
+void process(int width,int height,int bits,int format)
 {
 	unsigned char buf[4]="";
+	unsigned short r=0,g=0,b=0;
 	int row,col;
 	for (row = 0;row < height;row++)
 	{
@@ -33,7 +32,42 @@ void process(int width,int height,int bits)
 		{
 			fread(buf,bits,1,stdin);
 			if (row % 2 == 0 && col % 2 == 0)
-				fwrite(buf,bits,1,stdout);
+			{
+				if (format == 5)
+				{
+					buf[3] = buf[0];
+					buf[0] = buf[2];
+					buf[2] = buf[3];
+				}
+				if (format == 4)
+				{
+					r = ((buf[1] << 8 + buf[0]) & 0xF800) >> 11;
+					g = ((buf[1] << 8 + buf[0]) & 0x7E0) >> 5;
+					b = (buf[1] << 8 + buf[0]) & 0x1F;
+					buf[0] = (unsigned char)b;
+					buf[1] = (unsigned char)g;
+					buf[2] = (unsigned char)r;
+				}
+				if (format == 6)
+				{
+					r = ((buf[1] << 8 + buf[0]) & 0x7C00 ) >> 10;
+					g = ((buf[1] << 8 + buf[0]) & 0x3E0) >> 5;
+					b = (buf[1] << 8 + buf[0]) & 0x1F;
+					buf[0] = (unsigned char)b;
+					buf[1] = (unsigned char)g;
+					buf[2] = (unsigned char)r;
+				}
+				if (format == 7)
+				{
+					r = ((buf[1] << 8 + buf[0]) & 0xF00) >> 8;
+					g = ((buf[1] << 8 + buf[0]) & 0xF0) >> 4;
+					b = (buf[1] << 8 + buf[0]) & 0xF;
+					buf[0] = (unsigned char)b;
+					buf[1] = (unsigned char)g;
+					buf[2] = (unsigned char)r;
+				}
+				fwrite(buf,3,1,stdout);
+			}
 		}
 	}
 }
